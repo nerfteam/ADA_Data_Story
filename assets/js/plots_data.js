03a5c2cd20ff48bf909fb16874afe374d8aa3f87
Natalija Mitic <naca@ntec.ch>
2021-12-17 20:04:15 +0100

    Updating layout, adding new plots, matching, residuals

--- assets/js/plots_data.js
+++ assets/js/plots_data.js
@@ -2,8 +2,9 @@ const barcharts = ["barchart_test"];
 const pies = ["piechart_test", "pie_quotes_party", "pie_climate_quotes_party",
     "pie_politicians_party", "pie_gender_quotes", "pie_gender_cc_quotes",
     "pie_gender_democrats", "pie_gender_republicans",
-    "pie_positive", "pie_positive_cc", "pie_negative", "pie_negative_cc",
-    "pie_true", "pie_misinformation", "pie_misinfo_total"];
+    "pie_positive", "pie_positive_cc", "pie_negative_cc",
+    "pie_true", "pie_misinformation", "pie_misinfo_total",
+"pie_sentiment_republicans", "pie_sentiment_democrats"];
 const piesLabel = ["pie_politicians", "pie_quotes", "pie_climate_quotes"]
 const timeseries = ["BarackObama", "HillaryClinton", "BernieSanders", "DonaldTrump"]
 
